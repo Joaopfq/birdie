a372b0ab4c63c0d24f994005987155812dfaa2c4
Jpc <Joaop3595@gmail.com>
2025-04-09 01:23:18 -0300

    feat: add toggle dark mode

--- birdie/src/app/page.tsx
+++ birdie/src/app/page.tsx
@@ -1,15 +1,14 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton mode="modal"/>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className="m-4">
+      <h1>
+        home page
+      </h1>
     </div>
   );
 }
