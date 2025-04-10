be7c7dcae39b8fd7856b49a6e09838a7b60979be
Jpc <Joaop3595@gmail.com>
2025-04-10 17:07:24 -0300

    change next version to solve compatibility issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  weight: "100 900",
 });
-
-const geistMono = Geist_Mono({
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -39,22 +40,21 @@ export default function RootLayout({
           >
             <div className="min-h-screen">
               <Navbar />
+
               <main className="py-8">
+                {/* container to center the content */}
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:cols-span-3">
-                      sidebar
+                    <div className="hidden lg:block lg:col-span-3">
                     </div>
-                    <div className="lg:col-span-9">
-                      {children}
-                    </div>
+                    <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
               </main>
             </div>
           </ThemeProvider>
         </body>
-      </html>
+      </html>      
     </ClerkProvider>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
-import Link from 'next/link'
-import React from 'react'
-import DesktopNavbar from './DesktopNavbar'
-import MobileNavbar from './MobileNavbar'
+import Link from "next/link";
+import DesktopNavbar from "./DesktopNavbar";
+import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+  if (user) await syncUser();
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
@@ -19,7 +22,6 @@ function Navbar() {
         </div>
       </div>
     </nav>
-  )
+  );
 }
-
-export default Navbar
+export default Navbar;
