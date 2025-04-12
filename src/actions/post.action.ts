7526ea37ab2989bb9da5847017c8c792905bb7d9
Jpc <Joaop3595@gmail.com>
2025-04-12 00:05:24 -0300

    feat: add create post action

---
+++ src/actions/post.action.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { getDbUserId } from "./user.action";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(content: string, image: string){
+  try {
+
+    const userId = await getDbUserId();
+
+    if(!userId) return;
+
+    const post = await prisma.post.create({
+      data: {
+        content,
+        image,
+        authorId: userId,
+      },
+    });
+    
+    revalidatePath("/");
+
+    return {sucess:true, post}
+    
+  } catch (error) {
+
+    console.log("Failed to create post:", error);
+    return { sucess: false, error: "Failed to create post" };
+  }
+}
