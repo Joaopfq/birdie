8ab69f654e94a6a0bb8c56ef0288bb8dcd7e0e44
Jpc <Joaop3595@gmail.com>
2025-04-15 19:40:48 -0300

    feat: add notification provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "react-hot-toast";
+import { NotificationProvider } from "@/context/NotificationContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -41,21 +42,20 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <div className="min-h-screen">
-              <Navbar />
-
-              <main className="py-8">
-                {/* container to center the content */}
-                <div className="max-w-7xl mx-auto px-4">
-                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3">
-                      <Sidebar />
-                    </div>
-                    <div className="lg:col-span-9">{children}
-                      
+              <NotificationProvider>                    
+                <Navbar />
+                <main className="py-8">
+                  {/* container to center the content */}
+                  <div className="max-w-7xl mx-auto px-4">
+                    <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                      <div className="hidden lg:block lg:col-span-3">
+                        <Sidebar />
+                      </div>
+                        <div className="lg:col-span-9">{children}</div>
                     </div>
                   </div>
-                </div>
-              </main>
+                </main>
+              </NotificationProvider>
             </div>
             <Toaster />
           </ThemeProvider>
